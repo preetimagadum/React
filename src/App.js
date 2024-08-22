f4e68f050c6ef15cde0c1dd61c2938f70ecf6f56
preetimagadum <preetimagadum2@gmail.com>
2024-08-23 00:23:33 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { Fragment } from 'react'
- 
-import Users from './Userss/Users'
-let App=()=>{
-
-    return <Fragment>
-            <h1>App - Component</h1>
-            
-            <hr/>
-            <Users />
-            
-           </Fragment>
-    
+import React from 'react'
+import { Link,BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Services from './Pages/Services'
+import Contact from './Pages/Contact'
+import Users from './Pages/Users'
+const App = () => {
+  return <div>
+         <BrowserRouter>
+            <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
+                <Link to="/index" className='navbar-brand'>Login</Link>
+                <div className='ml-auto'>
+                    <ul className='navbar-nav'>
+                        <li><Link to="/index" className='nav-link'>Home</Link></li>
+                        <li><Link to="/about" className='nav-link'>About</Link></li>
+                        <li><Link to="/services" className='nav-link'>Services</Link></li>
+                        <li><Link to="/contact" className='nav-link'>Contact</Link></li>
+                        <li><Link to="/users" className='nav-link'>Users</Link></li>
+                    </ul>
+                </div>
+            </nav>
+            <Routes>
+                <Route path="index" element={<Home/>}/>
+                <Route path="about" element={<About/>}/>
+                <Route path="services" element={<Services/>}/>
+                <Route path="contact" element={<Contact/>}/>
+                <Route path="users" element={<Users/>}/>
+            </Routes>
+            </BrowserRouter>
+          </div>
 }
 
 export default App
---
+++ src/App1.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+ 
+import Users from './Userss/Users'
+let App=()=>{
+
+    return <Fragment>
+            <h1>App - Component</h1>
+            
+            <hr/>
+            <Users />
+            
+           </Fragment>
+    
+}
+
+export default App
